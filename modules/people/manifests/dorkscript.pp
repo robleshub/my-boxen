f0c829c0bb1969718784261c289f303a8c2c72ee
Arron Green <arrong@gmail.com>
2013-03-05 15:52:13 -0500

    testing the opposite of natural scroll

--- modules/people/manifests/dorkscript.pp
+++ modules/people/manifests/dorkscript.pp
@@ -72,7 +72,7 @@ class people::dorkscript{
   boxen::osx_defaults { "Disable 'natural scrolling'":
     key    => 'com.apple.swipescrolldirection',
     domain => 'NSGlobalDomain',
-    value  => 'false',
+    value  => 'true',
   }
   boxen::osx_defaults { 'Disable the "Are you sure you want to open this application?" dialog':
     key    => 'LSQuarantine',
